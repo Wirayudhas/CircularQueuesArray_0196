8fe2ddbe16258f495077cdf420af066969c288cd
Wirayudhas <arturwirayuda@gmail.com>
2024-05-30 11:19:43 +0700

    menampilkan tampilan isi antrian

--- CircularQueues_0196.cpp
+++ CircularQueues_0196.cpp
@@ -47,4 +47,27 @@ public:
 		}
 		cout << "\nThe element deleted from the queue is: " << queue_array[FRONT] << "\n";
 
+		//cek jika antrian hanya memiliki satu elemnet
+		if (FRONT == REAR) {
+			FRONT = -1;
+			REAR = -1;
+		}
+		else {
+			//jika element yang dihapus berada di posisi terakhir array, kembali ke awal array
+			if (FRONT == max - 1)
+				FRONT - 0;
+			else
+				FRONT = FRONT + 1;
+		}
+	}
+
+	void display() {
+		//cek apakah antrian kosong
+		if (FRONT == -1) {
+			cout << "Queue is empty\n";
+			return;
+		}
+
+		cout << "\nElement in the queue are...\n";
+
 		
